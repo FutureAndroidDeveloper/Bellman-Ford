f379a8b771c8a15dfe7f89f6c135a142bd0bc761
Kirill Klimenkov <KKlimenkov@qulix.com>
2020-11-16 21:17:30 +0300

    Добавлена возможность ставить вершины графа и соединять их

---
+++ MapViewTest/MapViewTest/CustomAnnotationView.swift
@@ -0,0 +1,51 @@
+//
+//  CustomAnnotationView.swift
+//  MapViewTest
+//
+//  Created by Klimenkov, Kirill on 16.11.2020.
+//  Copyright © 2020 Kirill Klimenkov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class CustomAnnotationView: MKAnnotationView {
+    private let annotationFrame = CGRect(x: 0, y: 0, width: 40, height: 40)
+    private let label: UILabel
+
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        self.label = UILabel(frame: annotationFrame.offsetBy(dx: 0, dy: -6))
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        self.frame = annotationFrame
+        self.label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        self.label.textColor = .white
+        self.label.textAlignment = .center
+        self.backgroundColor = .clear
+        self.addSubview(label)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) not implemented!")
+    }
+
+    public var number: UInt32 = 0 {
+        didSet {
+            self.label.text = String(number)
+        }
+    }
+
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+
+        context.beginPath()
+        context.move(to: CGPoint(x: rect.midX, y: rect.maxY))
+        context.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        context.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        context.closePath()
+
+        UIColor.blue.set()
+        context.fillPath()
+    }
+
+}
--- MapViewTest/MapViewTest/ViewController.swift
+++ MapViewTest/MapViewTest/ViewController.swift
@@ -16,14 +16,19 @@ class ViewController: UIViewController {
     private var index: Int = 0
     private var lastCoordinate: CLLocationCoordinate2D?
     
+    private var lpgr: UILongPressGestureRecognizer!
+    
+    private var activeVertix: MKAnnotation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setMapview()
         graph()
     }
     
-    private func setMapview(){
-        let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(gestureReconizer:)))
+    private func setMapview() {
+        lpgr = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(gestureReconizer:)))
+        lpgr.cancelsTouchesInView = true
         lpgr.minimumPressDuration = 0.5
         lpgr.delaysTouchesBegan = true
         lpgr.delegate = self
@@ -32,6 +37,7 @@ class ViewController: UIViewController {
     }
     
     private func addAnnotation(on coordinate: CLLocationCoordinate2D) {
+        
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
         annotation.title = "\(index)"
@@ -39,10 +45,17 @@ class ViewController: UIViewController {
         mapView.addAnnotation(annotation)
         
         if let lastCoordinate = lastCoordinate {
-            addLine(from: lastCoordinate, to: coordinate)
+            let myLastCoordinate = activeVertix?.coordinate
+            print(myLastCoordinate)
+//            addLine(from: lastCoordinate, to: coordinate)
+            addLine(from: myLastCoordinate!, to: coordinate)
+        } else {
+            activeVertix = annotation
         }
         index += 1
         lastCoordinate = coordinate
+        
+        
     }
     
     private func addLine(from begin: CLLocationCoordinate2D, to end: CLLocationCoordinate2D) {
@@ -81,18 +94,47 @@ class ViewController: UIViewController {
         print(shortestWeight)
         
     }
+    
+    
+    @objc func haha(gestureReconizer: UILongPressGestureRecognizer) {
+        print("LOX")
+    }
 }
 
 extension ViewController: UIGestureRecognizerDelegate {
+//    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+//        
+//        return true
+//    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
     @objc func handleLongPress(gestureReconizer: UILongPressGestureRecognizer) {
+        
+        
         if gestureReconizer.state != .ended {
-            let touchLocation = gestureReconizer.location(in: mapView)
+            var touchLocation = gestureReconizer.location(in: mapView)
+            
+            if let vertix = activeVertix, let myView = mapView.view(for: vertix) {
+                let loc = gestureReconizer.location(in: mapView)
+                let ccc = mapView.hitTest(loc, with: nil)
+                
+                if let cac = ccc as? MKAnnotationView {
+                    touchLocation = cac.center
+                    mapView.removeAnnotation(cac.annotation!)
+                }
+                
+            }
+            
             let locationCoordinate = mapView.convert(touchLocation, toCoordinateFrom: mapView)
             addAnnotation(on: locationCoordinate)
             print("Tapped at lat: \(locationCoordinate.latitude) long: \(locationCoordinate.longitude)")
             return
         }
         if gestureReconizer.state != .began {
+            
             return
         }
     }
@@ -107,5 +149,38 @@ extension ViewController: MKMapViewDelegate {
             return polylineRender
         }
         return MKOverlayRenderer(overlay: overlay)
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        activeVertix = view.annotation
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard annotation is MKPointAnnotation else { return nil }
+
+        let customAnnotationView = self.customAnnotationView(in: mapView, for: annotation)
+        customAnnotationView.number = arc4random_uniform(10)
+        customAnnotationView.accessibilityLabel = "\(customAnnotationView.number)"
+        
+        let abc = UILongPressGestureRecognizer()
+        abc.minimumPressDuration = 0.3
+        abc.require(toFail: lpgr)
+        abc.addTarget(self, action: #selector(haha(gestureReconizer:)))
+        customAnnotationView.addGestureRecognizer(abc)
+        return customAnnotationView
+    }
+    
+    private func customAnnotationView(in mapView: MKMapView, for annotation: MKAnnotation) -> CustomAnnotationView {
+        let identifier = "CustomAnnotationViewID"
+
+        if let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? CustomAnnotationView {
+            annotationView.annotation = annotation
+            return annotationView
+        } else {
+            let customAnnotationView = CustomAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            customAnnotationView.canShowCallout = true
+            return customAnnotationView
+        }
     }
 }
